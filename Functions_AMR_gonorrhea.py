3236d3876a590679b3c1cc28caf27798a2a9be9a
RachelMurray-Watson <rmurwat@gmail.com>
2023-03-13 09:28:57 +0000

    Changed the "get test train data" to allow oversampling and return prev cip R

--- Functions_AMR_gonorrhea.py
+++ Functions_AMR_gonorrhea.py
@@ -1,81 +1,110 @@
 from imblearn.over_sampling import RandomOverSampler
-from sklearn.model_selection import  RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score, matthews_corrcoef
 
 
-### Create functions for the gonorrhea AMR paper 
+### Create functions for the gonorrhea AMR paper
 
 
-def effective_unnecessary_threshold(threshold_seq, y_predict_proba, y_test, cipro_R_prevalence):
+def effective_unnecessary_threshold(
+    threshold_seq, y_predict_proba, y_test, cipro_R_prevalence
+):
 
     get_effective_threshold = []
-    incorrectly_get_X_threshold = [] #no bootstrapping, no 95% CI 
+    incorrectly_get_X_threshold = []  # no bootstrapping, no 95% CI
     sensitivity_threshold = []
     specificity_threshold = []
     for threshold in threshold_seq:
 
         y_predict_test = np.where(y_predict_proba[:, 1] > threshold, 1, 0)
 
-        tn_test , fp_test , fn_test , tp_test  = confusion_matrix(y_true=y_test, y_pred=y_predict_test).ravel()
+        tn_test, fp_test, fn_test, tp_test = confusion_matrix(
+            y_true=y_test, y_pred=y_predict_test
+        ).ravel()
 
-        sensitivity_test  = tp_test  / (tp_test   + fn_test )
-        specificity_test   = tn_test / (tn_test + fp_test )
+        sensitivity_test = tp_test / (tp_test + fn_test)
+        specificity_test = tn_test / (tn_test + fp_test)
 
-        sensitivity_threshold.append(sensitivity_test*100)
-        specificity_threshold.append(specificity_test*100)
-        get_effective_threshold.append(sensitivity_test * cipro_R_prevalence*100 + (100 -  cipro_R_prevalence*100)) #q_p
-        incorrectly_get_X_threshold.append((100 - cipro_R_prevalence*100) * (1 - specificity_test)) #c_p"
-    return(sensitivity_threshold, specificity_threshold, get_effective_threshold, incorrectly_get_X_threshold)
+        sensitivity_threshold.append(sensitivity_test * 100)
+        specificity_threshold.append(specificity_test * 100)
+        get_effective_threshold.append(
+            sensitivity_test * cipro_R_prevalence * 100
+            + (100 - cipro_R_prevalence * 100)
+        )  # q_p
+        incorrectly_get_X_threshold.append(
+            (100 - cipro_R_prevalence * 100) * (1 - specificity_test)
+        )  # c_p"
+    return (
+        sensitivity_threshold,
+        specificity_threshold,
+        get_effective_threshold,
+        incorrectly_get_X_threshold,
+    )
+
 
 def get_best_hyperparameters(model, cv, space, X_train, y_train):
-        search = RandomizedSearchCV(model, space, scoring='roc_auc', n_iter=1,  n_jobs=-1, cv=cv, random_state=1)
-        result = search.fit(X_train, y_train)
-        return(result.best_params_)
+    search = RandomizedSearchCV(
+        model, space, scoring="roc_auc", n_iter=1, n_jobs=-1, cv=cv, random_state=1
+    )
+    result = search.fit(X_train, y_train)
+    return result.best_params_
+
 
 def get_best_features(feature_names, model_fit, X_test, y_test):
-    PI = permutation_importance(model_fit, X_test, y_test, n_repeats = 10, random_state = 42)
+    PI = permutation_importance(
+        model_fit, X_test, y_test, n_repeats=10, random_state=42
+    )
     important_features = []
     for q in PI.importances_mean.argsort()[::-1]:
         if PI.importances_mean[q] - 2 * PI.importances_std[q] > 0:
-          important_features.append(feature_names[q]) #works cos they are in same order as the x columns
-    return(important_features)
+            important_features.append(
+                feature_names[q]
+            )  # works cos they are in same order as the x columns
+    return important_features
 
 
-oversample = RandomOverSampler(sampling_strategy = 0.5, random_state=42)
+oversample = RandomOverSampler(sampling_strategy=0.5, random_state=42)
+
 
 def get_test_train_data_overfit(CIP_data_no_drop, year, feature_names, oversample_size):
     years_train = np.array(range(year - 5, year))
 
-    # first do for all clinics 
-    train_data = CIP_data_no_drop.loc[CIP_data_no_drop['YEAR'].isin(years_train)]
-    X_train = train_data[feature_names] #need to consider all columns BEFORE feature engineering
-    y_train = 1 - train_data['Susceptible']
-    X_train, y_train = oversample.fit_resample(X_train,y_train)
-    #test
-    test_data = CIP_data_no_drop.loc[CIP_data_no_drop['YEAR'].isin([year])]
+    # first do for all clinics
+    train_data = CIP_data_no_drop.loc[CIP_data_no_drop["YEAR"].isin(years_train)]
+    X_train = train_data[
+        feature_names
+    ]  # need to consider all columns BEFORE feature engineering
+    y_train = 1 - train_data["Susceptible"]
+    X_train, y_train = oversample.fit_resample(X_train, y_train)
+    # test
+    test_data = CIP_data_no_drop.loc[CIP_data_no_drop["YEAR"].isin([year])]
     X_test = test_data[feature_names]
-    y_test = 1 - test_data['Susceptible']
-    cipro_R = y_test.sum()/len(y_test) 
-    X_test, y_test = oversample.fit_resample(X_test,y_test)
-    return(test_data, train_data, X_train, y_train, X_test, y_test, cipro_R)
+    y_test = 1 - test_data["Susceptible"]
+    cipro_R = y_test.sum() / len(y_test)
+    X_test, y_test = oversample.fit_resample(X_test, y_test)
+    return (test_data, train_data, X_train, y_train, X_test, y_test, cipro_R)
 
 
-def get_test_train_data(CIP_data_no_drop, year, feature_names):
-    years_train = np.array(range(year - 5, year))
+def get_test_train_data(CIP_data_no_drop, year, feature_names, years_train, model_type):
 
-    CIP_data_training_years = CIP_data_no_drop.loc[CIP_data_no_drop['YEAR'].isin(years_train)]
-    CIP_data_testing_years = CIP_data_no_drop.loc[CIP_data_no_drop['YEAR'].isin([year])]
-    # first do for all clinics 
-    train_data = CIP_data_no_drop.loc[CIP_data_no_drop['YEAR'].isin(years_train)]
-    X_train = train_data[feature_names] #need to consider all columns BEFORE feature engineering
-    y_train = 1 - train_data['Susceptible']
-    #test
-    test_data = CIP_data_no_drop.loc[CIP_data_no_drop['YEAR'].isin([year])]
+    train_data = CIP_data_no_drop.loc[CIP_data_no_drop["YEAR"].isin(years_train)]
+    X_train = train_data[
+        feature_names
+    ]  # need to consider all columns BEFORE feature engineering
+    y_train = 1 - train_data["Susceptible"]
+    # test
+    test_data = CIP_data_no_drop.loc[CIP_data_no_drop["YEAR"].isin([year])]
     X_test = test_data[feature_names]
-    y_test = 1 - test_data['Susceptible']
-    return(test_data, train_data, X_train, y_train, X_test, y_test)
+    y_test = 1 - test_data["Susceptible"]
+    cipro_R_prev = y_test.sum() / len(y_test)
+    if (model_type == 1) | (model_type == 2):
+        X_train, y_train = oversample.fit_resample(X_train, y_train)
+        X_test, y_test = oversample.fit_resample(X_test, y_test)
+
+    return (test_data, train_data, X_train, y_train, X_test, y_test, cipro_R_prev)
+
 
 def f1_mcc_score_threshold(threshold_seq, y_predict_proba, y_test):
 
@@ -87,15 +116,15 @@ def f1_mcc_score_threshold(threshold_seq, y_predict_proba, y_test):
 
         f1_score_seq.append(f1_score(y_test, y_predict))
         mcc_score_seq.append(matthews_corrcoef(y_test, y_predict))
-    return(f1_score_seq, mcc_score_seq)
+    return (f1_score_seq, mcc_score_seq)
 
 
 def encoder_for_GISP(data, column):
     encoder = OneHotEncoder()
-    encoder_categories  = encoder.fit(data[[column]]).categories_
-    encoder_categories  = encoder_categories[0].tolist()
+    encoder_categories = encoder.fit(data[[column]]).categories_
+    encoder_categories = encoder_categories[0].tolist()
     encoder_df = pd.DataFrame(encoder.fit_transform(data[[column]]).toarray())
     combined_data = data.join(encoder_df)
     col_names = list(data.columns) + encoder_categories[0:]
     combined_data.columns = col_names
-    return(combined_data)
+    return combined_data
